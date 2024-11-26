f346eb83bd34f688eac1418523b1e2ee35c2b50a
hasibhu <hasibul1987@gmail.com>
2024-11-26 02:51:42 +0100

    My bookig data have been shown in the myBooking route

--- src/app/myBookings/api/[email]/route.js
+++ src/app/myBookings/api/[email]/route.js
@@ -1,17 +1,17 @@
 import { connectDB } from "@/lib/connectDB";
 
 
-export const GET = async (request, {params}) => {
+export const GET = async (request, { params }) => {
     const db = await connectDB();
 
     const bookingCollection = db.collection('bookings');
      
     try {
       
-        const myBookings = await seviceCollection.find({email: params?.email});
+        const myBookings = await bookingCollection.find({ email: params?.email }).toArray();
     
-        return Response.json({myBookings})
+        return Response.json( myBookings )
     } catch (error) {
         console.log(error);
     }
-}
+};
--- src/app/myBookings/page.jsx
+++ src/app/myBookings/page.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+"use client"
+
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
 
 const MyBookings = () => {
+    const session = useSession();
+
+    const [bookings, setBookings] = useState();
+
+
+    const loadData = async () => {
+        const resp = await fetch(`http://localhost:3000/myBookings/api/${session?.data?.user?.email}`);
+   
+        const data = await resp.json();
+
+        console.log(data);
+        setBookings(data);
+    }
+
+
+   useEffect(() => {
+            if (session?.data?.user?.email) {
+                loadData();
+            }
+        }, [session]);
+
+
+    console.log(bookings);
     return (
-        <div>
+        <div className='container '>
+            <div>
+                    <Image
+                        className="object-cover"
+                        src={'/assets/images/about_us/parts.jpg'}
+                        alt={'Service Image'}
+                        width={1920}
+                        height={320}
+                    />
+                    
+            </div>
             
+
+
+            <h1 className='text-2xl text-center pt-10'>My Bookings</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Service Name</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                        {bookings?.map((booking, idx) => (
+                            <tr key={booking._id}>
+                                <th>{idx + 1}</th>
+                                <td>{booking.name}</td>
+                                <td>{booking.serviceTitle || 'N/A'}</td>
+                                <td>{booking.amount}</td>
+                                <td>{booking.date}</td>
+                                <td>
+                                    <button className="btn btn-sm btn-primary">Cancel</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+
+                </table>
+                </div>
+
+
+
         </div>
     );
 };
 
 export default MyBookings;
+
